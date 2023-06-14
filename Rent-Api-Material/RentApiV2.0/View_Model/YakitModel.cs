5b116fcac261e92250836bd303438c3cc8b65508
Glitch1269 <111871384+Glitch1269@users.noreply.github.com>
2023-06-14 15:46:28 +0300

    Add files via upload

---
+++ Rent-Api-Material/RentApiV2.0/View_Model/YakitModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RentApiV2._0.View_Model
+{
+    public class YakitModel
+    {
+        public string yakitId { get; set; }
+        public string yakitAdi { get; set; }
+    }
+}
